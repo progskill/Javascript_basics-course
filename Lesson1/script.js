70228363e466cd407a6202c520a98c8a50999a4e
Evans-mutuku <evansnyamai98@gmail.com>
2024-01-07 18:57:04 +0300

    Lesson one

--- Lesson1/script.js
+++ Lesson1/script.js
@@ -1,1 +1,9 @@
-console.log("Lesson One")
+// This is a single line Comments
+console.log("Lesson One");
+
+/*
+This is a multiple line Comments 
+console.log("Lesson 2");
+None of this is going to run!
+console.log("Your age is", 23);
+*/
