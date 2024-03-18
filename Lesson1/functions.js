2ef5caf49d1a08e75e055dc365269b5041964e9f
Evans-mutuku <evansnyamai98@gmail.com>
2024-03-18 16:35:32 +0300

    Lesson 2

--- Lesson1/functions.js
+++ Lesson1/functions.js
@@ -1,4 +1,20 @@
 // named functions
 
+function sum(a, b){
+    return a + b;
+}
+
+console.log(sum(5, 7))
+console.log(sum(4, 2))
+console.log(sum(2, 0))
+// sum()
 
 // Arrow functions
+
+const product = (c, d) => {
+
+    return c * d
+    // console.log("Hello world")
+}
+console.log(product(3, 7))
+// product()
